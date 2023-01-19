9db88b27b4b987ac1948e58b23ff71c8a0486fb3
Alaa Bawaneh <a.bawaneh@wecan.jo>
2023-01-19 16:27:28 +0300

    aaa

--- src/app/components/call-us/call-us.component.ts
+++ src/app/components/call-us/call-us.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import swal from 'sweetalert';
+
 
 @Component({
   selector: 'app-call-us',
@@ -10,6 +13,16 @@ export class CallUsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_d4j14ne', 'template_ubov4to', e.target as HTMLFormElement, 'BsucEti5Ae3z-3OM0')
+      .then((result: EmailJSResponseStatus) => {
+        swal("Good job!", "Your email was sent", "success");
+      }, (error) => {
+        swal("Oh noes!", "The AJAX request failed!", "error");
+      });
   }
 
 }
